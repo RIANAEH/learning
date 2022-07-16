d9b31b35a8d4ef380508faf7ba39453edcd79332
RIANAEH <ririhan217@gmail.com>
2022-07-17 04:44:18 +0900

    feat: [페어 타이머] 페어끼리 턴 교체 기능 구현

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,11 +1,15 @@
-import React , {useState} from 'react';
+import React , {useEffect, useState} from 'react';
 import SimpleTimer from './SimpleTimer/SimpleTimer';
 import InputTime from './InputTime';
+import Container from './Container';
 
 const Countdown = () => {
 
+    const crews = ['🐶', '🐱'];
+    const [now, setNow] = useState(true);
+
     const [isRunning, setRunning] = useState(false);
-    
+
     const timerValue = {
         minutes: 0,
         seconds: 0
@@ -19,12 +23,17 @@ const Countdown = () => {
     const stop = () => {
         setTimer(timerValue);
         setRunning(!isRunning);
+
+        setNow(!now);
     }
     
     const [ timer , setTimer ] = useState(timerValue);
     
     return (
         <div>
+            <Container m='10px 0' f='20px'>
+                <mark>드라이버: {now ? (crews[0]) : (crews[1])}</mark>
+            </Container>
             {isRunning ? (
                 <SimpleTimer 
                     minutes={timer.minutes} 
